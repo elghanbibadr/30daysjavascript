0053bcd8ecaec62ee654234edae59a88de123d4b
badr ghanbi <bghanbi50@gmail.com>
2022-11-30 17:27:02 +0100

    set up clock app project

---
+++ Javascript/main.js
@@ -0,0 +1,9 @@
+// let secondSlider=document.querySelector('.secondSlider');
+
+
+// let cc=0;
+// setInterval(()=>{
+// cc+=3;
+// secondSlider.style.transform=`rotate(${cc}deg)`;
+// console.log(cc)
+// },1000)
