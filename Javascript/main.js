1c9d24a65dae0b0f1bd9039668842ebb25cf49a2
badr ghanbi <bghanbi50@gmail.com>
2022-12-01 15:42:22 +0100

    create first demo of code

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,9 +1,37 @@
-// let secondSlider=document.querySelector('.secondSlider');
+// let currentDate=new Date();
 
 
-// let cc=0;
-// setInterval(()=>{
-// cc+=3;
-// secondSlider.style.transform=`rotate(${cc}deg)`;
-// console.log(cc)
-// },1000)
+let secondsSlider=document.querySelector('.secondSlider');
+let minuteSlider=document.querySelector('.minuteSlider');
+let hourSlider=document.querySelector('.hourSlider')
+let slides=document.querySelectorAll('.slide');
+
+let degreePerSecond=6;
+let degreePerHour=30;
+
+function rotateSlider(seconds,minute,hour){
+ let rotateSecondSliderBy=seconds*degreePerSecond;
+ let rotateMinuteSliderBy=minute*degreePerSecond;
+ let rotateHourSliderBy=hour*degreePerHour;
+secondsSlider.style.transform=`rotate(${270 + rotateSecondSliderBy}deg)`
+minuteSlider.style.transform=`rotate(${270 + rotateMinuteSliderBy}deg)`
+hourSlider.style.transform=`rotate(${270 + rotateHourSliderBy}deg)`
+}
+
+
+
+const convert24hourFormatTo12hour=(hour)=>{
+    if (!(hour >= 12)) return;
+    return hour% 12;
+}
+
+
+setInterval(()=>{
+    let currentDate=new Date();
+    let hour=currentDate.getHours();
+    let seconds=currentDate.getSeconds();
+     let minute=currentDate.getMinutes();
+     let convertedHour=convert24hourFormatTo12hour(hour)
+// let hour=currentDate.getHours();
+    rotateSlider(seconds,minute,convertedHour)
+,1000})
